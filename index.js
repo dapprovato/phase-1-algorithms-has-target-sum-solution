155ef4a6f719496c6e33bb163aff6826bcca9968
Daria Approvato <dariaapprovato@gmail.com>
2022-09-18 00:45:31 -0400

    commit tree 70043da4273e1cbcf2204613192f7407347f07ed

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set(); // initialize an empty Set
+  for (const number of array) {
+    const complement = target - number;
+
+    // .has returns true if the Set includes the complement
+    if (seenNumbers.has(complement)) return true;
+
+    // .add adds the number to the Set
+    seenNumbers.add(number);
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n)
 */
 
 /* 
@@ -29,6 +40,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 1, 1, 1], 1));
 }
 
 module.exports = hasTargetSum;
